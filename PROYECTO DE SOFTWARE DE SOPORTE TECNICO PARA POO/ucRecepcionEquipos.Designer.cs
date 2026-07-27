dac01c59f8fd1f0d51a96572e9f264e5d4e9dfcd
Luis Cruz <luiscruz200702@hotmail.com>
2026-07-26 23:44:31 -0500

    Arreglado parcialmente parte Registro

--- PROYECTO DE SOFTWARE DE SOPORTE TECNICO PARA POO/ucRecepcionEquipos.Designer.cs
+++ PROYECTO DE SOFTWARE DE SOPORTE TECNICO PARA POO/ucRecepcionEquipos.Designer.cs
@@ -28,17 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            pnlSuperior = new Panel();
+            pnlInformacion = new Panel();
+            pnlCentral = new Panel();
             SuspendLayout();
+            // 
+            // pnlSuperior
+            // 
+            pnlSuperior.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            pnlSuperior.AutoSize = true;
+            pnlSuperior.Location = new Point(0, 0);
+            pnlSuperior.Name = "pnlSuperior";
+            pnlSuperior.Size = new Size(1025, 59);
+            pnlSuperior.TabIndex = 0;
+            // 
+            // pnlInformacion
+            // 
+            pnlInformacion.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            pnlInformacion.AutoSize = true;
+            pnlInformacion.Location = new Point(0, 59);
+            pnlInformacion.Name = "pnlInformacion";
+            pnlInformacion.Size = new Size(1025, 59);
+            pnlInformacion.TabIndex = 1;
+            // 
+            // pnlCentral
+            // 
+            pnlCentral.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pnlCentral.AutoSize = true;
+            pnlCentral.Location = new Point(0, 118);
+            pnlCentral.Name = "pnlCentral";
+            pnlCentral.Size = new Size(1025, 466);
+            pnlCentral.TabIndex = 2;
             // 
             // ucRecepcionEquipos
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(pnlCentral);
+            Controls.Add(pnlInformacion);
+            Controls.Add(pnlSuperior);
             Name = "ucRecepcionEquipos";
             Size = new Size(1025, 686);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel pnlSuperior;
+        private Panel pnlInformacion;
+        private Panel pnlCentral;
     }
 }
