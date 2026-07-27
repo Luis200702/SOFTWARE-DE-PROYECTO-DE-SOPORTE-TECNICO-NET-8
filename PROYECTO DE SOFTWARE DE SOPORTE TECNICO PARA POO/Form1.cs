47d01a0beb4c2fe9410bce797c01e6f6c2c92f6e
Luis Cruz <luiscruz200702@hotmail.com>
2026-07-26 22:11:49 -0500

    Agregar archivos de proyecto.

---
+++ PROYECTO DE SOFTWARE DE SOPORTE TECNICO PARA POO/Form1.cs
@@ -0,0 +1,10 @@
+namespace PROYECTO_DE_SOFTWARE_DE_SOPORTE_TECNICO_PARA_POO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PROYECTO DE SOFTWARE DE SOPORTE TECNICO PARA POO/Program.cs
@@ -0,0 +1,17 @@
+namespace PROYECTO_DE_SOFTWARE_DE_SOPORTE_TECNICO_PARA_POO
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
