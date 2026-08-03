26b362bb2f09412ce94004744ccda0e56fe9c773
Marcorz04 <mrodriguezz@uteq.edu.ec>
2026-08-03 12:39:25 -0500

    cambios de nombres

--- PROYECTO DE SOFTWARE DE SOPORTE TECNICO PARA POO/ucHistorialClientes.Designer.cs
+++ PROYECTO DE SOFTWARE DE SOPORTE TECNICO PARA POO/ucHistorialClientes.Designer.cs
@@ -28,27 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle7 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle8 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
             materialButton1 = new ReaLTaiizor.Controls.MaterialButton();
             pictureBox1 = new PictureBox();
             label1 = new Label();
             panel4 = new Panel();
             splitContainer1 = new SplitContainer();
             panel1 = new Panel();
-            uiButton1 = new Sunny.UI.UIButton();
+            uibuscarcliente = new Sunny.UI.UIButton();
             txtBuscarCliente = new Sunny.UI.UITextBox();
             panel3 = new Panel();
-            uiButton2 = new Sunny.UI.UIButton();
-            label8 = new Label();
+            uibreautilizar = new Sunny.UI.UIButton();
+            lblvalor = new Label();
             label7 = new Label();
             label6 = new Label();
-            label5 = new Label();
-            label4 = new Label();
-            label3 = new Label();
-            label2 = new Label();
+            lblnvisitas = new Label();
+            lblcorreo = new Label();
+            lbltelefono = new Label();
+            lblNombre = new Label();
             dataGridView1 = new DataGridView();
             Orden = new DataGridViewTextBoxColumn();
             Fecha = new DataGridViewTextBoxColumn();
@@ -156,30 +156,30 @@
             // 
             // panel1
             // 
-            panel1.Controls.Add(uiButton1);
+            panel1.Controls.Add(uibuscarcliente);
             panel1.Controls.Add(txtBuscarCliente);
             panel1.Location = new Point(3, 13);
             panel1.Name = "panel1";
             panel1.Size = new Size(295, 109);
             panel1.TabIndex = 2;
             // 
-            // uiButton1
+            // uibuscarcliente
             // 
-            uiButton1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            uiButton1.FillColor = Color.FromArgb(20, 184, 166);
-            uiButton1.FillColor2 = Color.FromArgb(20, 184, 166);
-            uiButton1.FillDisableColor = Color.Empty;
-            uiButton1.Font = new Font("Segoe UI", 12F);
-            uiButton1.Location = new Point(27, 54);
-            uiButton1.MinimumSize = new Size(1, 1);
-            uiButton1.Name = "uiButton1";
-            uiButton1.RectColor = Color.Empty;
-            uiButton1.RectPressColor = Color.FromArgb(20, 184, 166);
-            uiButton1.RectSelectedColor = Color.FromArgb(20, 184, 166);
-            uiButton1.Size = new Size(246, 36);
-            uiButton1.TabIndex = 20;
-            uiButton1.Text = "Buscar Cliente";
-            uiButton1.TipsFont = new Font("Microsoft Sans Serif", 9F);
+            uibuscarcliente.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            uibuscarcliente.FillColor = Color.FromArgb(20, 184, 166);
+            uibuscarcliente.FillColor2 = Color.FromArgb(20, 184, 166);
+            uibuscarcliente.FillDisableColor = Color.Empty;
+            uibuscarcliente.Font = new Font("Segoe UI", 12F);
+            uibuscarcliente.Location = new Point(27, 54);
+            uibuscarcliente.MinimumSize = new Size(1, 1);
+            uibuscarcliente.Name = "uibuscarcliente";
+            uibuscarcliente.RectColor = Color.Empty;
+            uibuscarcliente.RectPressColor = Color.FromArgb(20, 184, 166);
+            uibuscarcliente.RectSelectedColor = Color.FromArgb(20, 184, 166);
+            uibuscarcliente.Size = new Size(246, 36);
+            uibuscarcliente.TabIndex = 20;
+            uibuscarcliente.Text = "Buscar Cliente";
+            uibuscarcliente.TipsFont = new Font("Microsoft Sans Serif", 9F);
             // 
             // txtBuscarCliente
             // 
@@ -207,49 +207,49 @@
             panel3.AutoScroll = true;
             panel3.AutoSize = true;
             panel3.BackColor = Color.FromArgb(23, 32, 50);
-            panel3.Controls.Add(uiButton2);
-            panel3.Controls.Add(label8);
+            panel3.Controls.Add(uibreautilizar);
+            panel3.Controls.Add(lblvalor);
             panel3.Controls.Add(label7);
             panel3.Controls.Add(label6);
-            panel3.Controls.Add(label5);
-            panel3.Controls.Add(label4);
-            panel3.Controls.Add(label3);
-            panel3.Controls.Add(label2);
+            panel3.Controls.Add(lblnvisitas);
+            panel3.Controls.Add(lblcorreo);
+            panel3.Controls.Add(lbltelefono);
+            panel3.Controls.Add(lblNombre);
             panel3.Location = new Point(-1, 5);
             panel3.Name = "panel3";
             panel3.Size = new Size(870, 107);
             panel3.TabIndex = 3;
             // 
-            // uiButton2
+            // uibreautilizar
             // 
-            uiButton2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            uiButton2.FillColor = Color.FromArgb(45, 212, 191);
-            uiButton2.FillColor2 = Color.FromArgb(45, 212, 191);
-            uiButton2.Font = new Font("Segoe UI", 12F);
-            uiButton2.Location = new Point(718, 39);
-            uiButton2.MinimumSize = new Size(1, 1);
-            uiButton2.Name = "uiButton2";
-            uiButton2.RectColor = Color.Empty;
-            uiButton2.RectDisableColor = Color.Empty;
-            uiButton2.RectHoverColor = Color.FromArgb(45, 212, 191);
-            uiButton2.RectPressColor = Color.FromArgb(45, 212, 191);
-            uiButton2.RectSelectedColor = Color.FromArgb(45, 212, 191);
-            uiButton2.Size = new Size(131, 36);
-            uiButton2.TabIndex = 21;
-            uiButton2.Text = "Reautilizar datos";
-            uiButton2.TipsFont = new Font("Microsoft Sans Serif", 9F);
+            uibreautilizar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            uibreautilizar.FillColor = Color.FromArgb(45, 212, 191);
+            uibreautilizar.FillColor2 = Color.FromArgb(45, 212, 191);
+            uibreautilizar.Font = new Font("Segoe UI", 12F);
+            uibreautilizar.Location = new Point(718, 39);
+            uibreautilizar.MinimumSize = new Size(1, 1);
+            uibreautilizar.Name = "uibreautilizar";
+            uibreautilizar.RectColor = Color.Empty;
+            uibreautilizar.RectDisableColor = Color.Empty;
+            uibreautilizar.RectHoverColor = Color.FromArgb(45, 212, 191);
+            uibreautilizar.RectPressColor = Color.FromArgb(45, 212, 191);
+            uibreautilizar.RectSelectedColor = Color.FromArgb(45, 212, 191);
+            uibreautilizar.Size = new Size(131, 36);
+            uibreautilizar.TabIndex = 21;
+            uibreautilizar.Text = "Reautilizar datos";
+            uibreautilizar.TipsFont = new Font("Microsoft Sans Serif", 9F);
             // 
-            // label8
+            // lblvalor
             // 
-            label8.AutoSize = true;
-            label8.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label8.ForeColor = Color.FromArgb(45, 212, 191);
-            label8.Location = new Point(640, 39);
-            label8.Name = "label8";
-            label8.Size = new Size(50, 21);
-            label8.TabIndex = 6;
-            label8.Text = "$ ###";
-            label8.Click += label8_Click;
+            lblvalor.AutoSize = true;
+            lblvalor.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblvalor.ForeColor = Color.FromArgb(45, 212, 191);
+            lblvalor.Location = new Point(640, 39);
+            lblvalor.Name = "lblvalor";
+            lblvalor.Size = new Size(50, 21);
+            lblvalor.TabIndex = 6;
+            lblvalor.Text = "$ ###";
+            lblvalor.Click += label8_Click;
             // 
             // label7
             // 
@@ -271,48 +271,48 @@
             label6.TabIndex = 4;
             label6.Text = "visitas";
             // 
-            // label5
+            // lblnvisitas
             // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label5.ForeColor = Color.White;
-            label5.Location = new Point(593, 39);
-            label5.Name = "label5";
-            label5.Size = new Size(19, 21);
-            label5.TabIndex = 3;
-            label5.Text = "#";
+            lblnvisitas.AutoSize = true;
+            lblnvisitas.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblnvisitas.ForeColor = Color.White;
+            lblnvisitas.Location = new Point(593, 39);
+            lblnvisitas.Name = "lblnvisitas";
+            lblnvisitas.Size = new Size(19, 21);
+            lblnvisitas.TabIndex = 3;
+            lblnvisitas.Text = "#";
             // 
-            // label4
+            // lblcorreo
             // 
-            label4.AutoSize = true;
-            label4.ForeColor = Color.FromArgb(148, 163, 184);
-            label4.Location = new Point(96, 73);
-            label4.Name = "label4";
-            label4.Size = new Size(75, 15);
-            label4.TabIndex = 2;
-            label4.Text = "@gmail.com";
-            label4.Click += label4_Click;
+            lblcorreo.AutoSize = true;
+            lblcorreo.ForeColor = Color.FromArgb(148, 163, 184);
+            lblcorreo.Location = new Point(96, 73);
+            lblcorreo.Name = "lblcorreo";
+            lblcorreo.Size = new Size(75, 15);
+            lblcorreo.TabIndex = 2;
+            lblcorreo.Text = "@gmail.com";
+            lblcorreo.Click += label4_Click;
             // 
-            // label3
+            // lbltelefono
             // 
-            label3.AutoSize = true;
-            label3.ForeColor = Color.FromArgb(148, 163, 184);
-            label3.Location = new Point(20, 73);
-            label3.Name = "label3";
-            label3.Size = new Size(70, 15);
-            label3.TabIndex = 1;
-            label3.Text = "#########";
+            lbltelefono.AutoSize = true;
+            lbltelefono.ForeColor = Color.FromArgb(148, 163, 184);
+            lbltelefono.Location = new Point(20, 73);
+            lbltelefono.Name = "lbltelefono";
+            lbltelefono.Size = new Size(70, 15);
+            lbltelefono.TabIndex = 1;
+            lbltelefono.Text = "#########";
             // 
-            // label2
+            // lblNombre
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.ForeColor = Color.White;
-            label2.Location = new Point(20, 39);
-            label2.Name = "label2";
-            label2.Size = new Size(67, 20);
-            label2.TabIndex = 0;
-            label2.Text = "Nombre";
+            lblNombre.AutoSize = true;
+            lblNombre.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblNombre.ForeColor = Color.White;
+            lblNombre.Location = new Point(20, 39);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(67, 20);
+            lblNombre.TabIndex = 0;
+            lblNombre.Text = "Nombre";
             // 
             // dataGridView1
             // 
@@ -321,47 +321,47 @@
             dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dataGridView1.BackgroundColor = Color.FromArgb(15, 23, 42);
-            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = Color.Coral;
-            dataGridViewCellStyle1.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle1.ForeColor = Color.Yellow;
-            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
-            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle5.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = Color.Coral;
+            dataGridViewCellStyle5.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle5.ForeColor = Color.Yellow;
+            dataGridViewCellStyle5.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Orden, Fecha, Dispositivo, Reparacion, Costo, Estado, Tecnico });
-            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = Color.FromArgb(15, 23, 42);
-            dataGridViewCellStyle3.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle3.ForeColor = Color.White;
-            dataGridViewCellStyle3.SelectionBackColor = Color.FromArgb(15, 23, 43);
-            dataGridViewCellStyle3.SelectionForeColor = Color.GhostWhite;
-            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
-            dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle7.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = Color.FromArgb(15, 23, 42);
+            dataGridViewCellStyle7.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle7.ForeColor = Color.White;
+            dataGridViewCellStyle7.SelectionBackColor = Color.FromArgb(15, 23, 43);
+            dataGridViewCellStyle7.SelectionForeColor = Color.GhostWhite;
+            dataGridViewCellStyle7.WrapMode = DataGridViewTriState.False;
+            dataGridView1.DefaultCellStyle = dataGridViewCellStyle7;
             dataGridView1.GridColor = Color.Gray;
             dataGridView1.Location = new Point(3, 110);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.ReadOnly = true;
-            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = SystemColors.ControlText;
-            dataGridViewCellStyle4.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            dataGridViewCellStyle4.ForeColor = Color.White;
-            dataGridViewCellStyle4.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle4.SelectionForeColor = SystemColors.GradientInactiveCaption;
-            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
-            dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle8.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = SystemColors.ControlText;
+            dataGridViewCellStyle8.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle8.ForeColor = Color.White;
+            dataGridViewCellStyle8.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle8.SelectionForeColor = SystemColors.GradientInactiveCaption;
+            dataGridViewCellStyle8.WrapMode = DataGridViewTriState.True;
+            dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle8;
             dataGridView1.RowHeadersVisible = false;
             dataGridView1.Size = new Size(866, 592);
             dataGridView1.TabIndex = 0;
             // 
             // Orden
             // 
-            dataGridViewCellStyle2.BackColor = Color.FromArgb(15, 23, 42);
-            dataGridViewCellStyle2.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            dataGridViewCellStyle2.ForeColor = Color.White;
-            dataGridViewCellStyle2.SelectionBackColor = Color.FromArgb(15, 23, 42);
-            Orden.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle6.BackColor = Color.FromArgb(15, 23, 42);
+            dataGridViewCellStyle6.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle6.ForeColor = Color.White;
+            dataGridViewCellStyle6.SelectionBackColor = Color.FromArgb(15, 23, 42);
+            Orden.DefaultCellStyle = dataGridViewCellStyle6;
             Orden.HeaderText = "ORDEN";
             Orden.Name = "Orden";
             Orden.ReadOnly = true;
@@ -445,15 +445,15 @@
         private Sunny.UI.UITextBox txtContraseña;
         private Sunny.UI.UIButton btnAcceder;
         private Sunny.UI.UITextBox txtBuscarCliente;
-        private Sunny.UI.UIButton uiButton1;
+        private Sunny.UI.UIButton uibuscarcliente;
         private Panel panel3;
-        private Label label3;
-        private Label label2;
-        private Label label8;
+        private Label lbltelefono;
+        private Label lblNombre;
+        private Label lblvalor;
         private Label label7;
         private Label label6;
-        private Label label5;
-        private Label label4;
-        private Sunny.UI.UIButton uiButton2;
+        private Label lblnvisitas;
+        private Label lblcorreo;
+        private Sunny.UI.UIButton uibreautilizar;
     }
 }
