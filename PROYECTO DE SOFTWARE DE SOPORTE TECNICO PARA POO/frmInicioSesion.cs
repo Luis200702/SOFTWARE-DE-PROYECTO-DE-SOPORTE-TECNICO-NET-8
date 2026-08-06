e5f33e81d10460e47f19479bfdb06382be7a8627
Luis Cruz <luiscruz200702@hotmail.com>
2026-08-05 20:46:19 -0500

    arreglsdo

--- PROYECTO DE SOFTWARE DE SOPORTE TECNICO PARA POO/frmInicioSesion.cs
+++ PROYECTO DE SOFTWARE DE SOPORTE TECNICO PARA POO/frmInicioSesion.cs
@@ -13,43 +13,9 @@ namespace PROYECTO_DE_SOFTWARE_DE_SOPORTE_TECNICO_PARA_POO
 
         private void btnAcceder_Click(object sender, EventArgs e)
         {
-            try
-            {
-                using (SqlConnection cn = conexion.ObtenerConexion())
-                {
-                    cn.Open();
-
-                    string consulta = @"SELECT COUNT(*) 
-                                FROM usuarios
-                                WHERE username = @usuario
-                                AND password = @password";
-
-                    SqlCommand cmd = new SqlCommand(consulta, cn);
-
-                    cmd.Parameters.AddWithValue("@usuario", txtUsuario.Text.Trim());
-                    cmd.Parameters.AddWithValue("@password", txtContraseña.Text.Trim());
-
-                    int resultado = Convert.ToInt32(cmd.ExecuteScalar());
-
-                    if (resultado > 0)
-                    {
-                        frmMenu menuPrincipal = new frmMenu();
-                        menuPrincipal.Show();
-                        this.Hide();
-                    }
-                    else
-                    {
-                        MessageBox.Show("Usuario o contraseña incorrectos",
-                            "Error",
-                            MessageBoxButtons.OK,
-                            MessageBoxIcon.Error);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            frmMenu menuPrincipal = new frmMenu();
+            menuPrincipal.Show();
+            this.Hide();
 
         }
 
