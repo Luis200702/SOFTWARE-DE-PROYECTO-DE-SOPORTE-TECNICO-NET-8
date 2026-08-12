d919fe8455601ca30019b1aad99a70e44e453dfc
SAHVIN <maldonadocruzjustin@gmail.com>
2026-08-11 22:16:34 -0500

    Hola 123

--- PROYECTO DE SOFTWARE DE SOPORTE TECNICO PARA POO/Properties/Resources.Designer.cs
+++ PROYECTO DE SOFTWARE DE SOPORTE TECNICO PARA POO/Properties/Resources.Designer.cs
@@ -283,26 +283,6 @@ namespace PROYECTO_DE_SOFTWARE_DE_SOPORTE_TECNICO_PARA_POO.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap TablerArrowsLeftRight {
-            get {
-                object obj = ResourceManager.GetObject("TablerArrowsLeftRight", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap TablerArrowsLeftRight__1_ {
-            get {
-                object obj = ResourceManager.GetObject("TablerArrowsLeftRight (1)", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap TablerSearch {
             get {
                 object obj = ResourceManager.GetObject("TablerSearch", resourceCulture);
