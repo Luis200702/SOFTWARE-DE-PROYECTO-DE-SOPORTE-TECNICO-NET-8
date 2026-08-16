21335f79c1bb87454410d24934ff33ec7f7b5816
Luis Cruz <luiscruz200702@hotmail.com>
2026-08-16 01:04:12 -0500

    xd

--- PROYECTO DE SOFTWARE DE SOPORTE TECNICO PARA POO/ucReportes.Designer.cs
+++ PROYECTO DE SOFTWARE DE SOPORTE TECNICO PARA POO/ucReportes.Designer.cs
@@ -28,17 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
+            pnlSuperior = new Panel();
+            pictureBox1 = new PictureBox();
+            lblTitulo = new Label();
+            pnlSuperior.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // pnlSuperior
+            // 
+            pnlSuperior.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            pnlSuperior.AutoSize = true;
+            pnlSuperior.BackColor = Color.White;
+            pnlSuperior.BorderStyle = BorderStyle.FixedSingle;
+            pnlSuperior.Controls.Add(pictureBox1);
+            pnlSuperior.Controls.Add(lblTitulo);
+            pnlSuperior.Location = new Point(-8, 0);
+            pnlSuperior.Name = "pnlSuperior";
+            pnlSuperior.Size = new Size(1236, 59);
+            pnlSuperior.TabIndex = 38;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(34, 13);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(36, 30);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // lblTitulo
+            // 
+            lblTitulo.Dock = DockStyle.Left;
+            lblTitulo.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitulo.ForeColor = Color.Black;
+            lblTitulo.Location = new Point(0, 0);
+            lblTitulo.Name = "lblTitulo";
+            lblTitulo.Size = new Size(231, 57);
+            lblTitulo.TabIndex = 1;
+            lblTitulo.Text = "Reportes";
+            lblTitulo.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // ucReportes
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(pnlSuperior);
             Name = "ucReportes";
-            Size = new Size(1025, 686);
+            Size = new Size(1123, 832);
+            pnlSuperior.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel pnlSuperior;
+        private PictureBox pictureBox1;
+        private Label lblTitulo;
     }
 }
